b673e0cee6de73471872fab8a3f5c5b35083c405
LiuWen <162258246+houli123@users.noreply.github.com>
2025-05-02 09:09:45 +0800

    complete the bit calculation

--- C/Code/2_15.c
+++ C/Code/2_15.c
@@ -3,9 +3,9 @@
 int main() {
     // 只使用位级和逻辑运算，编写一个 C 表达式，它等价千 x==y。换句话说，当 x 和 y 相等时它将返回 1, 否则就返回 0 。
     int x = 0x66, y = 0x39;
-    if ()
-        printf(1);
+    if ((x & y) == x)
+        printf("1");
     else
-        printf(0);
+        printf("0");
     return 0;
 }
